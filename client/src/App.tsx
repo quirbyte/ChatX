135c59e58ed25ec3eed9d0ba224dbb7934110274
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-03-02 22:14:24 +0530

    did basic backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import ChatArea from "./components/chatArea"
+import ChatArea from "./components/ChatArea"
 export default function App(){
   return <div className="bg-black text-white h-screen w-screen flex justify-center items-center">
     <ChatArea/>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,37 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets = [];
+interface User {
+  socket: WebSocket;
+  roomId: string;
+}
 
-wss.on("connection",(socket)=>{
-    allSockets.push(socket);
-    userCount++;
-    console.log("User connected #"+userCount);
-    socket.on("message",(message)=>{
-        console.log(message.toString());
-        for(let userSocket of allSockets){
-            userSocket.send(message.toString());
+let allUsers: User[] = [];
+
+wss.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    try {
+      const parsedMessage = JSON.parse(message as unknown as string);
+      if (parsedMessage.type === "join") {
+        allUsers.push({
+          socket,
+          roomId: parsedMessage.payload.roomId,
+        });
+      } else if (parsedMessage.type === "chat") {
+        let currentUser = allUsers.find((x) => x.socket === socket);
+        if (!currentUser) return;
+        for (const user of allUsers) {
+          if (user.roomId === currentUser.roomId) {
+            user.socket.send(parsedMessage.payload.message);
+          }
         }
-    })
-})
+      }
+    } catch (e) {
+      console.log("Invalid JSON");
+    }
+  });
+  socket.on("close", () => {
+    allUsers = allUsers.filter((x) => x.socket !== socket);
+  });
+});
