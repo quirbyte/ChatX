5c0e95cda74ed65f8f8f577ba641467a4731bc6d
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-05-06 17:34:00 +0530

    build: reset turborepo

--- README.md
+++
@@ -1,5 +0,0 @@
-------Today's Manifesto-----
-
-1. auth ui
-2. login/logout -> mobile no./otp system
-3. middleware
--- apps/ws-server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 3002 });
-
-wss.on("connection", (socket) => {
-  socket.send("Server Connected");
-});
