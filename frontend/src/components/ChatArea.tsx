b5237492d9b578c05e452910af7e99a9fa8eb1a3
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-05-13 19:24:23 +0530

    ui : add Leave Room Confirm Dialog

--- frontend/src/components/ChatArea.tsx
+++ frontend/src/components/ChatArea.tsx
@@ -1,10 +1,13 @@
-import { LogOut,Send } from "lucide-react"
+import { LogOut, Send } from "lucide-react"
+import { useState } from "react";
+import LeaveDialog from "./LeaveRoomDialog";
 
 export default function ChatArea() {
-    return <div className="flex flex-col h-full w-full text-white py-1 px-3">
+    const [leaveDialogOpen, setLeaveDialogOpen] = useState(false);
+    return <> <div className="flex flex-col h-full w-full text-white py-1 px-3">
         <div className="flex gap-3 items-center">
             <h1 className="uppercase text-xl font-bold tracking-wider text-zinc-300"># General</h1>
-            <div className="text-zinc-300 h-6 w-6 bg-red-900 rounded-full flex justify-center items-center">
+            <div onClick={()=>setLeaveDialogOpen(true)} className="text-zinc-300 h-6 w-6 bg-red-900 rounded-full flex justify-center items-center">
                 <LogOut size={13} />
             </div>
         </div>
@@ -15,22 +18,26 @@ export default function ChatArea() {
         </div>
 
         <div className="py-2 px-0 bg-zinc-950">
-                <div className="relative flex items-end bg-zinc-900 border border-zinc-800 rounded-2xl focus-within:border-zinc-600 transition-all p-2">
-                    <textarea 
-                        className="flex-1 bg-transparent border-none outline-none p-2 text-sm resize-none scrollbar-none min-h-11 max-h-32"
-                        placeholder="Send a message..."
-                        rows={1}
-                        onInput={(e) => {
-                            const target = e.target as HTMLTextAreaElement;
-                            target.style.height = 'inherit';
-                            target.style.height = `${target.scrollHeight}px`;
-                        }}
-                    />
-                    <button className="mb-1 mr-1 p-2 bg-inherit text-white rounded-xl hover:bg-zinc-800 transition-all active:scale-95">
-                        <Send size={18} />
-                    </button>
-                </div>
+            <div className="relative flex items-end bg-zinc-900 border border-zinc-800 rounded-2xl focus-within:border-zinc-600 transition-all p-2">
+                <textarea
+                    className="flex-1 bg-transparent border-none outline-none p-2 text-sm resize-none scrollbar-none min-h-11 max-h-32"
+                    placeholder="Send a message..."
+                    rows={1}
+                    onInput={(e) => {
+                        const target = e.target as HTMLTextAreaElement;
+                        target.style.height = 'inherit';
+                        target.style.height = `${target.scrollHeight}px`;
+                    }}
+                />
+                <button className="mb-1 mr-1 p-2 bg-inherit text-white rounded-xl hover:bg-green-800 transition-all active:scale-95">
+                    <Send size={18} />
+                </button>
             </div>
+        </div>
     </div>
+    {leaveDialogOpen && <div className="fixed inset-0 z-50 flex justify-center items-center bg-zinc-950/40 backdrop-blur-xs">
+        <LeaveDialog handleClose={()=>setLeaveDialogOpen(false)} />
+    </div>}
+    </>
 }
 
--- frontend/src/components/LeaveRoomDialog.tsx
+++ frontend/src/components/LeaveRoomDialog.tsx
@@ -1,6 +1,16 @@
 
-export default function LeaveDialog(){
-    return <div>
-        
+export interface LeaveCardProps{
+    handleClose : () => void;
+
+}
+
+export default function LeaveDialog({handleClose}:LeaveCardProps){
+    return <div className="relative h-60 w-60 bg-zinc-900 text-white rounded-xl flex justify-center items-center">
+        <h1 className="absolute top-1 text-3xl font-bold text-center">Confirm Leave</h1>
+        <p>Do yo want to leave the room?</p>
+        <div className="flex absolute bottom-0 w-full">
+            <button onClick={handleClose} className="w-[50%] cursor-pointer hover:opacity-90 h-10 bg-zinc-700 rounded-bl-xl">No</button>
+            <button className="w-[50%] h-10 bg-red-800 cursor-pointer hover:opacity-90 rounded-br-xl">Yes</button>
+        </div>
     </div>
 }
